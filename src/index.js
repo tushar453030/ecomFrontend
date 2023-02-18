a37974d1421d941e4e88cec263a0976d7d86f44b
tushar453030 <tushar4530@gmail.com>
2023-02-19 00:06:16 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const root = document.getElementById('root'); // <- This is the //correct method call for React version 17
+render(<App />, root);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
